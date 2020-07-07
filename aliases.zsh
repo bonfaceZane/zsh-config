afb85b1dce37a87738d7c7a72d38efbf45e1f51c
zaneObwoni <zaneobwoni@gmail.com>
2020-07-07 14:24:47 +0200

    initial commit

---
+++ aliases.zsh
@@ -0,0 +1,14 @@
+alias lg='lazygit'
+alias gpu_watch="watch -n -1 nvidia-smi"
+alias zconfig="nvim ~/.zshrc"
+alias ohmyzsh="nvim ~/.oh-my-zsh"
+alias f="ranger"
+alias v="nvim"
+# use this with media keys later
+#alias play="spotify play"
+#alias pause="spotify pause"
+#alias next="spotify next"
+#alias prev="spotify prev"
+alias repos='ranger ~/Repos'
+alias zrc='nvim ~/.zshrc'
+alias exp='nvim ~/.config/zsh/exports.zsh'
